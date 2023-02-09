7f0b598927cbcfb37755f6f3e4e5edbb91a94f43
student student <student@mgok.local>
2023-02-09 12:27:53 +0300

    структура проекта

---
+++ Controller/Pagination/Navigation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtonSystems.Controller.Pagination
+{
+    internal class Navigation
+    {
+    }
+}
